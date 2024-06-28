4346985b434b755fbc47a95c23090b3a45fe4781
ryan pujo <pujotriantoro@gmail.com>
2024-06-28 23:22:37 +0700

    add findBySKU to ProductRepo

--- src/main/java/com/praim/inventory/product/repositories/ProductRepo.java
+++ src/main/java/com/praim/inventory/product/repositories/ProductRepo.java
@@ -4,10 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.praim.inventory.product.entities.Product;
-import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface ProductRepo extends CrudRepository<Product, Long>{
-  public List<Product> findByName(String name);
+  public Optional<Product> findBySKU(String sku);
 }
