ae285249be95b2f40a37720146b87d5d64b11024
ryan pujo <pujotriantoro@gmail.com>
2024-06-29 08:09:32 +0700

    delete getTotalStock, add totalStock, createdAt and updatedAt field

--- src/main/java/com/praim/inventory/product/entities/ProductInventory.java
+++ src/main/java/com/praim/inventory/product/entities/ProductInventory.java
@@ -3,7 +3,10 @@ package com.praim.inventory.product.entities;
 import com.praim.inventory.warehouse.entities.Warehouse;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity(name = "product_inventory")
@@ -30,10 +33,16 @@ public class ProductInventory {
     @EqualsAndHashCode.Exclude
     private Warehouse warehouse;
 
+    @Column(nullable = false)
+    private int totalStock;
+
     @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductVariant> productVariants;
 
-    public int getTotalStock() {
-        return productVariants.stream().mapToInt(ProductVariant::getStock).sum();
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
