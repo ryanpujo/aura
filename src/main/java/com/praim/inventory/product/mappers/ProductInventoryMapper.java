3d7b344e0d916e6baeacc8e271f419848b8c915f
ryan pujo <pujotriantoro@gmail.com>
2024-06-29 08:10:29 +0700

    add new ProductInventoryMapper class

---
+++ src/main/java/com/praim/inventory/product/mappers/ProductInventoryMapper.java
@@ -0,0 +1,17 @@
+package com.praim.inventory.product.mappers;
+
+import com.praim.inventory.product.dtos.ProductInventoryDTO;
+import com.praim.inventory.product.entities.ProductInventory;
+import com.praim.inventory.warehouse.mappers.WarehouseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(uses = {ProductVariantMapper.class, WarehouseMapper.class})
+public interface ProductInventoryMapper {
+
+    @Mapping(target = "product", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    ProductInventory toProductInventory(ProductInventoryDTO dto);
+
+    ProductInventoryDTO toProductInventoryDTO(ProductInventory productInventory);
+}
